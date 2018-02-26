8c0b9ee52500b71b99b7686125c0c88d8369e75a
Marlon Copeland <mcopeland26@msn.com>
2018-02-25 22:32:33 -0500

    tab navigation for home page

--- Veracakes.MVC.Web/Controllers/HomeController.cs
+++ Veracakes.MVC.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,17 +13,29 @@ namespace Veracakes.MVC.Web.Controllers
     {
         public ActionResult Index()
         {
-            MagnifyingGlass x = new MagnifyingGlass();
-            int tops = x.Rings;
+            IndexViewModel idx = new IndexViewModel();
+            ViewBag.Message = "Your next best cake starts here.";
 
-            return View();
+            string strImagesPath = Server.MapPath("~/Content/Images/cakePics/");
+            CakePage cakePage = new CakePage();
+            DirectoryInfo cakeImageDirectory = new DirectoryInfo(strImagesPath);
+            cakePage.lstCakes = cakeImageDirectory.GetFiles().Select(x => x.Name).ToList();
+
+            idx.cakePage = cakePage;
+
+            return View(idx);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your next best cake starts here.";
 
-            return View();
+            string strImagesPath = Server.MapPath("~/Content/Images/cakePics/");
+            CakePage cakePageViewModel = new CakePage();
+            DirectoryInfo cakeImageDirectory = new DirectoryInfo(strImagesPath);
+            cakePageViewModel.lstCakes = cakeImageDirectory.GetFiles().Select(x => x.Name).ToList();
+
+            return View(cakePageViewModel);
         }
 
         public ActionResult Contact()
---
+++ Veracakes.MVC.Web/ViewModels/CakePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Veracakes.MVC.Web
+{
+    public class CakePage
+    {
+        public CakePage()
+        {
+            lstCakes = new List<string>();
+            strDescription = "";
+        }
+
+       public  List<string> lstCakes { get; set; }
+       public string strDescription { get; set; }
+    }
+}
